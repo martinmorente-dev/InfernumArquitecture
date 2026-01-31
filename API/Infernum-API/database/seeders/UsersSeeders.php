894b86052a4d4c0be236ff4837fdad275a453d5c
Martin Morente Vargas <martin.dev45@gmail.com>
2026-01-31 20:36:43 +0100

    Update
    Things done:
    
    --> Creation of the Migration User
    
    --> Creation of the Model User
    
    --> Creation the seeder & factory for the user
    
    TODO:
    
    I need to do the routes of the API and search how to handle tokens

--- API/Infernum-API/database/seeders/UsersSeeders.php
+++ API/Infernum-API/database/seeders/UsersSeeders.php
@@ -4,18 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-class DatabaseSeeder extends Seeder
+class UsersSeeders extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        $this->call([
-            UsersSeeders::class
-        ]);
+        User::factory()->count(10)->create();
     }
 }
