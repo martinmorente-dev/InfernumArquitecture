74ff3b4c75e8ecd5bb530f8338c3bf5cb494dfac
Martin Morente Vargas <martin.dev45@gmail.com>
2026-04-23 20:30:07 +0200

    Update
    Thing done:
    
    --> Added the load balancer
    
    TODO:
    
    Correct errors with the load balancer

--- deploy/Front.tf
+++ deploy/Front.tf
@@ -72,9 +72,111 @@ resource "aws_route53_record" "front-record" {
 }
 
 
-
 /****************** Code deploy **********************************/
 
+/******************* Load Balancer **********************************/
+
+resource "aws_lb" "front_lb" {
+  name               = "frontend-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.front-group.id]
+  // add subnets
+
+  tags = {
+    Name = "frontend-lb"
+  }
+
+}
+
+# Target groups
+resource "aws_lb_target_group" "front-http" {
+  name        = "front-http-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.vpc.id
+  target_type = "instance"
+
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/"
+    port                = "80"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "front-http-tg"
+  }
+}
+
+resource "aws_lb_target_group" "front-https" {
+  name        = "front-455-tg"
+  port        = 455
+  protocol    = "TCP"
+  vpc_id      = data.aws_vpc.vpc.id
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    port                = "455"
+    protocol            = "TCP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "front-455-tg"
+  }
+}
+
+# Listener HTTP 80
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.front_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front-http.arn
+  }
+}
+
+# Listener HTTPS 455
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.front_lb.arn
+  port              = "455"
+  protocol          = "HTTPS"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front-https.arn
+  }
+}
+
+
+# Registrar front (httpp https)
+
+resource "aws_lb_target_group_attachment" "front-http-group" {
+  target_group_arn = aws_lb_target_group.front-http.arn
+  target_id        = aws_instance.Front.id
+  port             = 80
+}
+
+
+resource "aws_lb_target_group_attachment" "front-https-group" {
+  target_group_arn = aws_lb_target_group.front-https.arn
+  target_id        = aws_instance.Front.id
+  port             = 455
+}
 
 resource "aws_codedeploy_app" "frontend" {
   name = "frontend-app"
@@ -92,5 +194,4 @@ resource "aws_codedeploy_deployment_group" "frontend" {
   }
 
 }
-
 
